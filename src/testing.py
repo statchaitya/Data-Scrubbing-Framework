d9dc00ddd648d33d9410e5bf95e0572591c6a476
statchaitya <statchaitya@gmail.com>
2020-07-02 10:08:58 +0530

    testing.py

---
+++ src/testing.py
@@ -0,0 +1,7 @@
+# ================================================================================ #
+# -- Testing of the scrub functions with different random datasets -- #
+
+# Dataset 1 - bank marketing
+# Define membership, range and data type dicts
+# Make adjustments to membership function currently supports GENDER only
+# Run the code, check the results
